d8d400f74c1cd4cd3492bdb56aa6a448cb6dd4d8
Your Name <you@example.com>
2022-12-08 23:22:46 +0200

    добавил мидлварю на проброс соединения с базой и добавление пользователя в базу1111

--- utils/db_connect.py
+++ utils/db_connect.py
@@ -5,7 +5,7 @@ class Request:
     def __init__(self, connector: psycopg_pool.AsyncConnectionPool.connection):
         self.connector = connector
 
-    async def add_user(self, user_id, user_name, chat_id, user_email, login_time):
+    async def add_user(self, user_id, user_name, chat_id, login_time):
         query = f"INSERT INTO users (user_id, user_name, chat_id, login_time) VALUES ({user_id}, '{user_name}', {chat_id}, '{login_time}') " \
                 f"ON CONFLICT (user_id) DO UPDATE SET user_name='{user_name}'"
         await self.connector.execute(query)
