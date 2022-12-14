3673d2b5d2c1bb7f36b8e26fa4407e1e0bb56550
Your Name <you@example.com>
2022-12-14 23:35:53 +0200

    начал делать вывод мемов33333

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from utils.utils import get_pagination_keyboard, phraze_list, get_pagination_list, PagesCallbackFactory, func_chunk, StaticMedia,
+from utils.utils import get_pagination_keyboard, phraze_list, get_pagination_list, PagesCallbackFactory, func_chunk, StaticMedia
 from utils.http_client import *
